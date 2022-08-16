630cca6737951a96a747a2710f513903bcc70b80
kien.letrung <kienle26074@gmail.com>
2022-08-16 23:27:14 +0700

    first commit

---
+++ src/pages/Login.js
@@ -0,0 +1,22 @@
+import React, { useRef, useState, useEffect } from "react";
+
+const Login = () => {
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [user, setUser] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, pwd]);
+  return <div></div>;
+};
+
+export default Login;
